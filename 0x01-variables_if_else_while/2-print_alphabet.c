32ceb784af477301cc94bcba435a9460dea4a000
Mcdharmie <damilola.afolayan07@gmail.com>
2023-10-06 23:05:22 +0000

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - here is a function
+ *Return: 0
+ */
+int main(void)
+{
+char i;
+
+ for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+
+}
