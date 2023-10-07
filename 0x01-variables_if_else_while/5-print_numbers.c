f2a166f9432b11f276919b11615be53f10ab64ba
Mcdharmie <damilola.afolayan07@gmail.com>
2023-10-07 01:08:42 +0000

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
+
 /**
- *main - printing the alphabet
- *Return: Always zero
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
+{
+int i;
 
+for (i = 0; i < 10; i++)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-if ((letter = 'e' && letter != 'q'))
-putchar(letter);
+printf("%d", i);
 }
 putchar('\n');
 return (0);
